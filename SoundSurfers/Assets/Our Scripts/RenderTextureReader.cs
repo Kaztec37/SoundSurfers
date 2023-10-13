ea45e75f5d2c42bdf407ed37298b13ef49d17ab8
FabrizioDeFiore <fabriziodefiore@gmail.com>
2023-10-13 23:12:58 +0100

    3DEnvColorSoundificationScene + updateOnVideoConversion

--- SoundSurfers/Assets/Our Scripts/RenderTextureReader.cs
+++ SoundSurfers/Assets/Our Scripts/RenderTextureReader.cs
@@ -2,6 +2,116 @@ using UnityEngine;
 
 public class RenderTextureReader : MonoBehaviour
 {
+    private Camera mainCamera;
+    CsoundUnity csound;
+    int r;
+    float g;
+    int b;
+
+    //private Texture2D destTexture;
+    //Texture2D rTex;
+    RenderTexture rTex;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        csound = GetComponent<CsoundUnity>();
+        r = 50;
+        g = 0.2f;
+        b = 50;
+
+        Texture2D tex = new Texture2D(3840, 2160, TextureFormat.RGBA32, false);
+        RenderTexture.active = rTex;
+        tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
+        tex.Apply();
+    }
+
+    void Update()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        r = 50;
+        g = 0.2f;
+        b = 50;
+        csound.SetChannel("red", r);
+        csound.SetChannel("green", g);
+        csound.SetChannel("blue", b);
+        Debug.Log("RGB: " + r + ", " + g + ", " + b);
+
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Renderer rend = hit.collider.GetComponent<Renderer>();
+            if (rend != null)
+            {
+                Vector2 pixelUV = hit.textureCoord;
+                Texture2D tex = (Texture2D)rend.material.mainTexture;
+                
+                //Graphics.CopyTexture(tex, rTex);
+                
+                int x = (int)(pixelUV.x * tex.width);
+                int y = (int)(pixelUV.y * tex.height);
+
+                Color pixelColor = tex.GetPixel(x, y);
+
+                Debug.Log($"Getting Pixel value at {pixelUV}");
+                r = (int)(pixelColor.r * 255);
+                g = pixelColor.g ;
+                b = (int)(pixelColor.b * 255);
+                Debug.Log("RGB: " + r + ", " + g + ", " + b);
+                if (r < 1 && g == 0 && b < 1){
+                    r = 2; g = 0.5f; b = 2  ;
+                }
+                csound.SetChannel("red", r);
+                csound.SetChannel("green", g);
+                csound.SetChannel("blue", b);
+
+
+            } else {
+                Debug.Log("No Renderer found");
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+
+
+
+
     public RenderTexture renderTexture;
     public Vector2 textureSize = new Vector2(3840, 2160); 
 
@@ -47,119 +157,6 @@ public class RenderTextureReader : MonoBehaviour
 
             // Output the RGB values
             Debug.Log("R: " + pixelColor.r + " G: " + pixelColor.g + " B: " + pixelColor.b);
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    private Camera mainCamera;
-    CsoundUnity csound;
-    int r;
-    float g;
-    int b;
-
-    //private Texture2D destTexture;
-    //Texture2D rTex;
-    RenderTexture rTex;
-
-    void Start()
-    {
-        mainCamera = Camera.main;
-        csound = GetComponent<CsoundUnity>();
-        r = 50;
-        g = 0.2f;
-        b = 50;
-
-        // rTex = new Texture2D(3840, 2160, TextureFormat.RGBA32, false);
-        RenderTexture.active = rTex;
-    }
-
-    void Update()
-    {
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        r = 50;
-        g = 0.2f;
-        b = 50;
-        csound.SetChannel("red", r);
-        csound.SetChannel("green", g);
-        csound.SetChannel("blue", b);
-        Debug.Log("RGB: " + r + ", " + g + ", " + b);
-
-
-        if (Physics.Raycast(ray, out hit))
-        {
-            Renderer rend = hit.collider.GetComponent<Renderer>();
-            if (rend != null)
-            {
-                Vector2 pixelUV = hit.textureCoord;
-                Texture2D tex = (Texture2D)rend.material.mainTexture;
-                
-                //Graphics.CopyTexture(tex, destTexture);
-                
-                int x = (int)(pixelUV.x * tex.width);
-                int y = (int)(pixelUV.y * tex.height);
-
-                Color pixelColor = tex.GetPixel(x, y);
-
-                //Debug.Log($"Getting Pixel value at {pixelUV}");
-                r = (int)(pixelColor.r * 255);
-                g = pixelColor.g ;
-                b = (int)(pixelColor.b * 255);
-                Debug.Log("RGB: " + r + ", " + g + ", " + b);
-                if (r < 1 && g == 0 && b < 1){
-                    r = 2; g = 0.5f; b = 2  ;
-                }
-                csound.SetChannel("red", r);
-                csound.SetChannel("green", g);
-                csound.SetChannel("blue", b);
-
-
-            } else {
-                Debug.Log("No Renderer found");
-
-            }
         }
     }
     */
